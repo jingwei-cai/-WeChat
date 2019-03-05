db90456ae9f2b308160bfe5e8c748bac34dce29b
guoyang <1097085674@qq.com>
2019-03-05 23:28:34 +0800

    字体图标组件

---
+++ src/components/icon.vue
@@ -0,0 +1,41 @@
+<template>
+  <span
+    class="iconfont"
+    :class="type"
+    :style="{ color: color, 'font-size': fontSize }"
+    @click="iconClick"
+  ></span>
+</template>
+
+<script>
+export default {
+  props: {
+    /**
+     * 图标类型
+     */
+    type: String,
+
+    /**
+     * 图标颜色
+     */
+    color: String,
+
+    /**
+     * 图标大小
+     */
+    size: String
+  },
+  computed: {
+    fontSize() {
+      return this.size + "px";
+    }
+  },
+  methods: {
+    iconClick() {
+      this.$emit("click");
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped></style>
